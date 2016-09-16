9b42ff0e0611348f5e2afb1f8674c2d0ae4e0893
Pedro Martinez <pmartinez1085@gmail.com>
2016-09-16 15:57:59 +0200

    Array

--- indextemplate.php
+++ indextemplate.php
@@ -1,3 +1,23 @@
 <?php
-$greeting = "Hola" . $_GET['nom'];
-require 'index.php';
+
+
+//$greeting = "Hola" . $_GET['nom'] . "!";
+//require 'index.php';
+
+
+//$nom = array();
+
+$noms = [
+    'Sergi',
+    'Xavi',
+    'Pedro'
+];
+
+$array = ['Sergi', 'Xavi', 'Pedro'];   //Intentarem no fer aquest format si es un array llarg
+
+
+foreach ($names as $name){
+
+    echo "$name";
+
+}
