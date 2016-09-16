603faa269250d1bb01d6cd74081f9f00601dc800
Pedro Martinez <pmartinez1085@gmail.com>
2016-09-16 16:40:18 +0200

    array associatiu

--- index.php
+++ index.php
@@ -6,4 +6,24 @@ $noms = [
     'Pedro'
 ];
 
+// ARRAYS ASSOCIATIUS
+
+$person = [
+    'name' => 'Pedro',
+    'sn1' => 'Martínez',
+    'sn2' => 'Segura',
+    'edat' => '30',
+    'married' => yes
+];
+
+//
+//die('Xivato');
+//echo "NO S'EXECUTA MAI";
+//dd();
+//
+//
+//
+//var_dump($person);
+
+
 require 'index.template.php';
