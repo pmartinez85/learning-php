e0c1407f0bab9c2f80d31f7a32012a58c6d19c04
Pedro Martinez <pmartinez1085@gmail.com>
2016-09-20 19:51:51 +0200

    segon connect

--- index.php
+++ index.php
@@ -7,9 +7,22 @@ require 'Task.php';
 
 //PDO: Php Data Object library/biblioteca
 
-$pdo = new PDO('mysql:host=127.0.0.1;dbname=prova', 'root','');
+try {
 
-require 'index.template.php';
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=prova', 'root','');
+
+
+}
+
+catch (PDOexception $e){
+    die("ha hagut un error durant la connexió" . $e->getMessage());
+}
+
+
+$query = $pdo->prepare('select * from todos');
+$query->execute();
+var_dump($query->fetchAll());
+//require 'index.template.php';
 
 
 
