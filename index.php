1c7258b16e5e56caa30bb980604d4f265592e85a
Pedro Martinez <pmartinez1085@gmail.com>
2016-09-20 19:31:21 +0200

    primer connect

--- index.php
+++ index.php
@@ -3,8 +3,17 @@
 
 require 'function.php';
 
+require 'Task.php';
+
+//PDO: Php Data Object library/biblioteca
+
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=prova', 'root','');
+
 require 'index.template.php';
 
-require 'Task.php';
+
+
+
+
 
 
