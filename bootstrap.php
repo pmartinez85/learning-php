485ff0fb1b64f9ed255e44aa51e563b5f87958f9
Pedro Martinez <pmartinez1085@gmail.com>
2016-09-22 20:53:01 +0200

    bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'function.php';
+
+require 'Task.php';
+
+require 'database/QueryBuilder.php';
+
+require 'database/Connection.php';
+
+$config = require 'config/database.php';
+$message = require 'config/database.php';
+
+$pdo = Connection::make($config);
+
+$query = new QueryBuilder($pdo);
--- persons.php
+++ persons.php
@@ -1,21 +1,7 @@
 <?php
-<?php
 
+require "bottstrap.php";
 
-require 'function.php';
-
-require 'Task.php';
-
-require 'database/QueryBuilder.php';
-
-require 'database/Connection.php';
-
-$config = require 'config/database.php';
-$message = require 'config/database.php';
-
-$pdo = Connection::make($config);
-
-$query = new QueryBuilder($pdo);
 $tasks = $query->all('Persons');
 
 
