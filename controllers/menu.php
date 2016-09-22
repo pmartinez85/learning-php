cc35aa74490d2dce4ee50a2bd8b88463324efc5b
Pedro Martinez <pmartinez1085@gmail.com>
2016-09-22 21:52:25 +0200

    menus3

---
+++ controllers/menu.php
@@ -0,0 +1,12 @@
+<?php require 'menu.php' ?>;
+
+<nav>
+ul>li*4
+<ul>
+                <li><a href=""></a>Tasques</li>
+                <li><a href="persons"></a>Persones</li>
+                <li><a href="about"></a>About</li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+
+        </nav>
--- index.php
+++ index.php
@@ -2,10 +2,29 @@
 
 require "Core/bootstrap.php";
 
-$tasks = $query->all('todos');
+//$tasks = $query->all('todos');
 
-require 'index.template.php';
 
+
+$routes = [
+    ''          =>  'controllers/persons.php',
+    'persons'   =>  'controllers/persons',
+    'about'     =>  'controllers/about',
+    'contact'   =>  'controllers/contact',
+
+
+
+];
+
+$uri = (trim($_SERVER['REQUEST_URI'],'/'));
+
+if (array_key_exists($uri,$routes) ) {
+    require $routes[$uri];
+
+
+} else {
+    throw new Exception("No s'ha trobat la pagina");
+}
 
 
 
